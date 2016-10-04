47344140c2b2c4af0a826cd423320f80ae0210da
Nate Doyle <nathan.doyle26@gmail.com>
2016-10-04 16:38:50 -0600

    fixed what I think was an import conflict so the page works at least.

--- app/views.py
+++ app/views.py
@@ -1,24 +1,10 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from app import app
 from flask import render_template
-import create_page
-app = Flask(__name__)
-db = PyMongo(app)
 
-@app.route("/")
+@app.route('/')
+@app.route('/index')
 def index():
     user = {'nickname': 'Nate'}  # fake user
-    return '''
-    <html>
-    <head>
-    <title>Home Page</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['nickname'] + '''</h1>
-    </body>
-    </html>
-    '''
-
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
